1a73f08531111018e17403ff57e2fa8ca20b3be7
Valevach.90 <Valevach.90@mail.ru>
2021-05-11 15:56:35 +0300

    11.05.2021

---
+++ src/main/java/com/epam/rd/autocode/floodfill/WorkSpace.java
@@ -0,0 +1,44 @@
+package com.epam.rd.autocode.floodfill;
+
+
+public class WorkSpace implements FloodFill, FloodLogger {
+
+    @Override
+    public void flood(String map, FloodLogger logger) {
+        int lineLength = map.split("\n")[0].length() + 1;
+        logger.log(map);
+        if (map.indexOf(LAND) > -1) {
+            char[] tempMap = map.toCharArray();
+            int lastIndex = 0;
+            int indexOfWater;
+            while ((indexOfWater = map.indexOf(WATER, lastIndex)) >= 0 && lastIndex < map.length()) {
+                int rightIndex = indexOfWater + 1;
+                int leftIndex = indexOfWater - 1;
+                int upIndex = indexOfWater - lineLength;
+                int downIndex = indexOfWater + lineLength;
+                int currentLine = indexOfWater / lineLength;
+                if ((rightIndex - currentLine * lineLength) < lineLength - 1) {
+                    tempMap[rightIndex] = WATER;
+                }
+                if ((leftIndex - currentLine * lineLength) >= 0) {
+                    tempMap[leftIndex] = WATER;
+                }
+                if (upIndex >= 0) {
+                    tempMap[upIndex] = WATER;
+                }
+                if (downIndex < map.length()) {
+                    tempMap[downIndex] = WATER;
+                }
+                lastIndex = indexOfWater + 1;
+            }
+            String newMap = String.valueOf(tempMap);
+            flood(newMap, logger);
+        }
+    }
+
+
+    @Override
+    public void log(String floodState) {
+
+    }
+}
